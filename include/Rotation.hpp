1f3b007a601e6444b665a5d178305ac23aa7b73d
jimmy4345 <jimmy4345@gmail.com>
2024-10-12 16:11:27 +0800

    add C_ne

---
+++ include/Rotation.hpp
@@ -0,0 +1,26 @@
+#ifndef ROTATION_HPP
+#define ROTATION_HPP
+
+#include "Global_defs.hpp"
+
+class Rotation
+{
+public:
+    // Rotation matrix
+    static Matrix3d C_ne(const Vector3d &Pos_LLA);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // ROTATION_HPP
---
+++ src/Rotation.cpp
@@ -0,0 +1,17 @@
+#include "Rotation.hpp"
+
+/* Transformation matrix of navigation frame -> earth frame */
+Matrix3d Rotation::C_ne(const Vector3d &Pos_LLA)
+{
+    double s_lat = std::sin(Pos_LLA[0]);
+    double c_lat = std::cos(Pos_LLA[0]);
+    double s_lon = std::sin(Pos_LLA[1]);
+    double c_lon = std::cos(Pos_LLA[1]);
+
+    Matrix3d C_ne;
+    C_ne << -s_lat * c_lon, -s_lat * s_lon, c_lat,
+         -s_lon,           c_lon,           0,
+         -c_lat * c_lon, -c_lat * s_lon, -s_lat;
+
+    return C_ne;
+}
